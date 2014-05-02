fd8febb0d10db0a98218e9c5ee16a034832d20af
cdupras <charlie@duprasville.com>
2014-05-01 21:07:10 -0600

    first grid

--- first_try.js
+++ first_try.js
@@ -1,5 +1,49 @@
+var grid = [
+    [2048,1024,512,256],
+    [128,64,32,16],
+    [8,4,2,0],
+    [0,0,0,0]
+    ];
+
+var drawTile = function(num, gridY, gridX) {
+  //draw one grid tile here
+  var margin = 5;
+  var tileSize = 90;
+  
+  var tileX = margin + (gridX * margin * 2) + (gridX * tileSize);
+  var tileY = margin + (gridY * margin * 2) + (gridY * tileSize);
+  
+  rect(tileX, tileY, tileSize, tileSize);
+
+  fill(0, 0, 0);
+  textSize(25);
+  if (0 !== num) {
+    text(num, tileX + 25, tileY+ 75);
+  }
+  noFill();
+};
+
+for (var gridY=0; gridY<4; gridY++) {
+    for (var gridX=0; gridX<4; gridX++) {
+        drawTile(grid[gridY][gridX], gridY, gridX);
+    }
+}
+/*
+drawTile(grid[0][0], 0, 0);
+drawTile(grid[0][1], 0, 1);
+drawTile(grid[0][2], 0, 2);
+drawTile(grid[0][3], 0, 3);
+
+drawTile(grid[1][0], 1, 0);
+drawTile(grid[1][1], 1, 1);
+drawTile(grid[1][2], 1, 2);
+drawTile(grid[1][3], 1, 3);
+*/
+
+
 //2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048!
-var two = 4;
+
+/*var two = 4;
 var four = 4;
 var squareSize = 90;
 var squareMargin = 5 ;
@@ -12,9 +56,4 @@ while (squareY < 400) {
     }
     squareY += squareSize + 2*squareMargin;
 }
-/*
--tell computer what to do
--get #s on screen
--for arrays are the rows
--
 */
